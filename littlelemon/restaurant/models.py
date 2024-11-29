b11d8fa81b9b928c860e5f2bc464f6866ab0bc46
Stephanie Odoom <styzyl18@gmail.com>
2024-11-29 02:20:53 +0000

    mysql connection

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class BookingTable(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guest = models.IntegerField()
+    booking_date = models.DateTimeField()
+
+class MenuTable(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
